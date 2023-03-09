6bfa7347cf7ced06e752aa9758cd746848e700a4
Richard Marks <ccpsceo@gmail.com>
2023-03-09 12:26:56 -0600

    added basic invaders in static formation

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,105 @@ constexpr int SCREEN_HEIGHT = 240;
 constexpr int WINDOW_WIDTH = SCREEN_WIDTH * 4;
 constexpr int WINDOW_HEIGHT = SCREEN_HEIGHT * 4;
 
+constexpr int INVADER_SPACING = 4;
+constexpr int NUM_INVADERS_ACROSS = 10;
+constexpr int NUM_INVADERS_DOWN = 4;
+constexpr int NUM_INVADERS = NUM_INVADERS_ACROSS * NUM_INVADERS_DOWN;
+constexpr int INVADER_WIDTH = 16;
+constexpr int INVADER_HEIGHT = 16;
+
+class Invader
+{
+public:
+  bool alive{false};
+  float xPos;
+  float yPos;
+  float xVel;
+  float yVel;
+  SDL_Color color{0, 0xff, 0, 0xff};
+  SDL_Rect rect{-100, -100, INVADER_WIDTH, INVADER_HEIGHT};
+  void update(float deltaTime)
+  {
+    if (!alive)
+    {
+      return;
+    }
+    xPos += xVel * deltaTime;
+    yPos += yVel * deltaTime;
+    rect.x = static_cast<int>(xPos);
+    rect.y = static_cast<int>(yPos);
+  }
+  void draw(SDL_Renderer *renderer)
+  {
+    if (!alive)
+    {
+      return;
+    }
+    SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
+    SDL_RenderFillRect(renderer, &rect);
+  }
+};
+
+class Invaders
+{
+  float time{0};
+  float nextMovement{0.7};
+  Invader *invaders{nullptr};
+
+public:
+  Invaders()
+  {
+    invaders = new Invader[NUM_INVADERS];
+  }
+  ~Invaders()
+  {
+    delete[] invaders;
+  }
+  void getIntoFormation()
+  {
+    int xGap = INVADER_WIDTH + INVADER_SPACING;
+    int yGap = INVADER_HEIGHT + INVADER_SPACING;
+    for (int i = 0; i < NUM_INVADERS; i++)
+    {
+      Invader &obj = invaders[i];
+      // move to column
+      obj.xPos = static_cast<int>((i % NUM_INVADERS_ACROSS) * xGap);
+      // move to row
+      obj.yPos = static_cast<int>((i / NUM_INVADERS_ACROSS) * yGap);
+      // move to center x form
+      obj.xPos += (SCREEN_WIDTH - (xGap * NUM_INVADERS_ACROSS)) / 2;
+      // move down an invader height
+      // obj.yPos += (SCREEN_HEIGHT - (yGap * NUM_INVADERS_DOWN)) / 2;
+      // move up an invader height
+      obj.yPos += INVADER_HEIGHT;
+
+      obj.alive = true;
+    }
+  }
+
+  void update(float deltaTime)
+  {
+    time += deltaTime;
+    if (time >= nextMovement)
+    {
+      time -= nextMovement;
+      for (int i = 0; i < NUM_INVADERS; i++)
+      {
+        Invader &obj = invaders[i];
+        obj.update(deltaTime);
+      }
+    }
+  }
+  void draw(SDL_Renderer *renderer)
+  {
+    for (int i = 0; i < NUM_INVADERS; i++)
+    {
+      Invader &obj = invaders[i];
+      obj.draw(renderer);
+    }
+  }
+};
+
 int main()
 {
   SDL_Init(SDL_INIT_EVERYTHING);
@@ -15,6 +114,10 @@ int main()
   SDL_Event event;
   bool running = true;
   float lastTime = static_cast<float>(SDL_GetTicks());
+
+  Invaders invaders;
+
+  invaders.getIntoFormation();
 
   while (running)
   {
@@ -42,9 +145,11 @@ int main()
     lastTime = currentTime;
 
     // update
+    invaders.update(deltaTime);
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0xff);
     SDL_RenderClear(renderer);
     // render
+    invaders.draw(renderer);
     SDL_RenderPresent(renderer);
   }
 
